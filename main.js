0eeae1e22889d15681200509bcebc3f4db8f0568
sallygirl157 <102963086+sallygirl157@users.noreply.github.com>
2023-10-26 09:19:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ function tocaSomPom(){
 
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom  ;
+//document.querySelector('.tecla_pom').onclick = toca
+document.querySelectorAll('.tecla')
+
+const listaDeTeclas
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play('');
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+while(contador < 9){
+    listaDeTeclas[contador].onclick = function(){
+    tocaSom('#som_tecla_clap');
+    };
+
+    contador = contador + 1;
+}
+
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
